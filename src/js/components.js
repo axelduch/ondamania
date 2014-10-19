983e68b76fb7d1cf40574272456b78518a913e92
Axel Duch <axlduch@gmail.com>
2014-10-19 23:35:19 +0200

    Update used components and systems

--- src/js/components.js
+++ src/js/components.js
@@ -1,10 +1,15 @@
 module.exports = {
     dedicated: {
-        appearance: require('./components/dedicated/appearance.js'),
+        mass: require('./components/dedicated/mass.js'),
         position: require('./components/dedicated/position.js'),
+        velocity: require('./components/dedicated/velocity.js'),
+        acceleration: require('./components/dedicated/acceleration.js'),
+        hitbox: require('./components/dedicated/hitbox.js'),
+        appearance: require('./components/dedicated/appearance.js'),
         context2d: require('./components/dedicated/context2d.js')
     },
     shared: {
+        gravity: require('./components/shared/gravity.js'),
         time: require('./components/shared/time.js')
     }
 };
--- src/js/systems.js
+++ src/js/systems.js
@@ -1,3 +1,4 @@
 module.exports = {
-    render: require('./systems/render.js')
+    render: require('./systems/render.js'),
+    physics: require('./systems/physics.js')
 };
