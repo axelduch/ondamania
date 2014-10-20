3a3ae447ea332addb9a0eb649a6787e2f1f595a8
Axel Duch <axlduch@gmail.com>
2014-10-20 23:32:56 +0200

    Take rotation into account for rendering

--- src/js/systems/render.js
+++ src/js/systems/render.js
@@ -5,16 +5,27 @@ module.exports = function render(entities) {
         var entity = entities[id];
         var appearance = entity.components.appearance;
         var position = entity.components.position;
+        var rotation = entity.components.rotation;
         var context2d = entity.components.context2d;
 
         if (appearance && position && context2d) {
 
+
             var context = context2d.value;
+            var centerX = position.x + 0.5 * appearance.width;
+            var centerY = position.y + 0.5 * appearance.height;
 
             if (previousContext !== context) {
                 context.fillStyle = '#FFFFFF';
                 context.fillRect(0, 0, context.canvas.width, context.canvas.height);
                 previousContext = context;
+            }
+
+            if (rotation) {
+                context.save();
+                context.translate(centerX, centerY);
+                context.rotate(rotation.value);
+                context.translate(-centerX, -centerY);
             }
 
             context.fillStyle = 'rgba(' + [
@@ -25,6 +36,10 @@ module.exports = function render(entities) {
             ] + ')';
 
             context.fillRect(position.x, position.y, appearance.width, appearance.height);
+
+            if (rotation && rotation.value) {
+                context.restore();
+            }
         }
     }
 };
