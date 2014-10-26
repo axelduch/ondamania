e7eb174d9380ca4c5bcdbcf346b07c8d8731120a
Axel Duch <axlduch@gmail.com>
2014-10-26 14:55:14 +0100

    Added center of mass for mass component

--- src/js/components/dedicated/mass.js
+++ src/js/components/dedicated/mass.js
@@ -3,6 +3,10 @@ module.exports = function mass(params) {
 
     return {
         name: 'mass',
-        value: +params.value || 1
+        value: +params.value || 1,
+        center: params.center || {
+            x: 0.5,
+            y: 0.5
+        }
     };
 };
